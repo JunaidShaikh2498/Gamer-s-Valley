179ab336fefc523e71f652d5e14609fc89b8e5f4
ChahatAhuja <chahatahuja8@gmail.com>
2024-02-10 12:53:01 +0530

    product list page added

---
+++ src/products/ProductList.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import './ProdList.css';
+import pic from './mobile1.webp';
+
+const products = [
+  { id: 1, name: 'Product 1', price: '$10',image:pic,description:'Nice phone!!'},
+  { id: 2, name: 'Product 2', price: '$20',image:pic,description:'Nice phone!!' },
+  { id: 3, name: 'Product 3', price: '$30',image:pic,description:'Nice phone!!' },
+  { id: 4, name: 'Product 4', price: '$40',image:pic,description:'Nice phone!!' },
+  { id: 5, name: 'Product 5', price: '$50',image:pic,description:'Nice phone!!' },
+  { id: 6, name: 'Product 6', price: '$60',image:pic,description:'Nice phone!!' },
+  { id: 7, name: 'Product 7', price: '$60',image:pic,description:'Nice phone!!' },
+  { id: 8, name: 'Product 8', price: '$60',image:pic,description:'Nice phone!!' }
+
+];
+
+const ProductCard = ({ product }) => (
+  <div className="product-card">
+    <div className="product-image"> <img src={product.image}/> </div>
+    <div className="product-name">{product.name}</div>
+    <div className="product-price">{product.price}</div>
+    <div className="product-description">{product.description}</div>
+  </div>
+);
+
+const ProductList = () => {
+  const productCards = products.map((product) => (
+    <ProductCard key={product.id} product={product} />
+  ));
+
+  const rows = productCards.reduce((result, row, index) => {
+    if (index % 3 === 0) {
+      result.push([]);
+    }
+    result[result.length - 1].push(row);
+    return result;
+  }, []);
+
+  return (
+    <div className="product-list">
+      {rows.map((row, index) => (
+        <div key={index} className="product-row-group">
+          {row}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const ProductPage = () => {
+  return (
+    <div className="product-page">
+      <h1>Product Page</h1>
+      <ProductList />
+    </div>
+  );
+};
+
+export default ProductPage;
