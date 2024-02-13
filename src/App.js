6feaf8038b2bd6dc0ea1951aad2b8b2e7f040baf
ChahatAhuja <chahatahuja8@gmail.com>
2024-02-13 08:46:38 +0530

    ui updated after customer login

--- src/App.js
+++ src/App.js
@@ -5,27 +5,30 @@ import CustomerDash from './LoginnRegister/CustomerDash';
 import { Provider } from 'react-redux';
 import store from './store';
 import RegisterCustomer from './LoginnRegister/RegisterCustomer';
-import ProductList from './products/ProductList';
+import CategoryList from './products/CategoryList';
 import { Navbar } from './Navbar/Navbar';
 import { About } from './Navbar/About';
 import { Register } from './Navbar/Register';
+import pic from './Photos/Dashboard.webp';
+import './BackgroundImg.css';
 
+import { Browse } from './products/Browse';
 function App() {
   return (
     <>
     <Navbar/>
     <Provider store={store}>
-    <div className="App">
-        
+    <div className="App" style={{background:
+    "url(./Photos/Dashboard.webp) no-repeat center center fixed"}}>
         <Routes>
           <Route path='/re' element={<RegisterExpert/>}/>
           <Route path='/rc' element={<RegisterCustomer/>}/>
           <Route path='/login' element={<Login/>}/>
           <Route path='/dashboard' element={<CustomerDash/>}/>
-          <Route path='/prod' element={<ProductList/>}/>
           <Route path='/about' element={<About/>}/>
           <Route path='/register' element={<Register/>}/>
-          <Route path='/home' element={<ProductList/>}/>
+          <Route path='/home' element={<CategoryList/>}/>
+          <Route path='/browse_cat' element={<Browse/>}/>
 
         </Routes>
 
